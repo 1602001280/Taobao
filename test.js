409ddc9e7ac0d17c934800fb530f1394c53b3b73
iHx <754212163@qq.com>
2019-12-08 08:07:30 +0800

    Update test.js
    text 随便扔骰子

--- test.js
+++ test.js
@@ -1,3 +1,19 @@
+sleep(5000);
+for (let index = 0; index < 100; index++) {
+    //const element = array[index];
+    toastLog(index);
+    sleep(3000);
+    click(543, 1767);
+    sleep(500);
+    click(409, 1700);
+    sleep(500);
+    click(409, 1655);
+}
+
+
+
+
+
 /*
 app.launch("com.taobao.taobao");
 app.startActivity("com.taobao.tao.TBMainActivity");
@@ -13,7 +29,7 @@ app.startActivity({
     data : ""
 })
 */
-
+/*
 sleep(500);sleep(500);sleep(500);sleep(500);sleep(500);sleep(500);
 
 sleep(500);
@@ -25,3 +41,4 @@ swipe(500, 500, 500, 1500, 1000);
 sleep(500);
 swipe(500, 500, 500, 1500, 1000);
 sleep(500);
+*/
