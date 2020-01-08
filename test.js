10c2ebe4d7eb830c8c308d23871e232aac0dd8dc
iHx <754212163@qq.com>
2020-01-08 13:36:22 +0800

    UPDATE

--- test.js
+++ test.js
@@ -1,3 +1,277 @@
+
+
+
+
+function findLog(button, word) {
+    if (button) {
+        console.log("获取" + word + "成功");
+        console.log("Text = " + button.text());
+        console.log(button.bounds());
+        return button;
+    } else {
+        console.error("获取" + word + "失败");
+        return 0;
+    }
+}
+
+
+function findButton(word) {
+    let button = null;
+    for (let index = 0; index < 3; index++) {
+        button = desc(word).findOne(500);
+        if (button) {
+            break;
+        }
+        button = text(word).findOne(500);
+        if (button) {
+            break;
+        }
+    }
+    return findLog(button, word);
+}
+
+function findandClick(str) {
+    button = findButton(str);
+    if (button) {
+        if (button.clickable()) {
+            click(button.bounds().centerX(), button.bounds().centerY());
+            sleep(500);
+            console.log('aaa');
+            return 1;
+        } else {
+            click(button.bounds().centerX(), button.bounds().centerY());
+            sleep(500);
+            console.log('bbb');
+            return 1;
+        }
+    }
+    else {
+        console.error(str + "未点击");
+        console.log('ccc');
+        return 0;
+    }
+}
+
+var users = { "c***3": 0, "黑***5": 0, "芍***离": 0 };
+
+accountSwitch();
+function accountSwitch() {
+    for (let user in users) {
+        if (user == "") {
+            console.error("无账户");
+            return 0;
+        }
+    }
+
+    let act = currentActivity();
+    let tbmain = "com.taobao.tao.TBMainActivity";
+    if (act != tbmain) {
+        initAPP();
+    }
+
+    /*
+    function findandClick(str) {
+        button = findButton(str);
+        if (button) {
+            if (button.clickable()) {
+                click(button);
+                return 1;
+            } else {
+                click(button.bounds().centerX(), button.bounds().centerY());
+                sleep(500);
+                return 1;
+            }
+        }
+        else {
+            console.error(str + "未点击");
+            return 0;
+        }
+    }
+    */
+
+    // 我的淘宝 首页 设置 clickable为true但是点了没用 有用的是里面的child(0) 但是clickable为false点不了
+    button = findButton("我的淘宝");
+    if (button) {
+        click(button.bounds().centerX(), button.bounds().centerY());
+        sleep(500);
+    }
+    else {
+        console.error("账户切换失败");
+        return 0;
+    }
+
+    button = findButton("设置");
+    if (button) {
+        click(button.bounds().centerX(), button.bounds().centerY());
+        sleep(500);
+    }
+    else {
+        console.error("账户切换失败");
+        return 0;
+    }
+
+    waitForActivity("com.taobao.mytaobao.setting.TaobaoSettingActivity");
+
+    button = findButton("切换账户");
+    if (button) {
+        click(button.bounds().centerX(), button.bounds().centerY());
+        sleep(500);
+    }
+    else {
+        console.error("账户切换失败");
+        return 0;
+    }
+
+    button = findButton("当前登录");
+    if (button) {
+        let currentuser = button.parent().child(button.indexInParent() - 1).text();
+        console.log("当前用户" + currentuser);
+        users[currentuser] = 1;
+        for (let user in users) {
+            if (users[user] == 0) {
+                console.info("切换账户" + user);
+                button = findButton(user);
+                if (button) {
+                    click(button.bounds().centerX(), button.bounds().centerY());
+                    sleep(500);
+                    console.info("切换账户成功");
+                    return 1;
+                }
+                else {
+                    console.error("账户切换失败");
+                    return 0;
+                }
+            }
+        }
+        console.error("完成所有账户");
+        return 0;
+    }
+    else {
+        console.error("账户切换失败");
+        return 0;
+    }
+}
+
+
+
+
+
+/*function allfun() {
+    this.fun1 = function () {
+        console.log('aaa');
+    }
+    this.fun2 = function () {
+        console.log('bbb');
+    }
+}
+allfun();//及时执行
+
+var funs = new allfun();//实例化及时函数
+
+var f1 = new funs.fun1();//方法一实例化
+//var f2 = new funs.fun2();//方法二实例化
+//运行测试
+
+
+
+/*
+var users = {"aaa": 0};
+
+for (let user in users) {
+    if (user != "") {
+        console.log(user);
+    }
+    
+}
+
+/*
+    let act = currentActivity();
+    let tbmain="com.taobao.tao.TBMainActivity";
+    if (act!=tbmain) {
+        back();
+    }else{
+        console.log('aaa');
+    }
+
+
+/*
+* 创建函数--闭包
+*/
+/*
+function foo(){ 
+    var val1=18; 
+    console.log('val1');
+    function baz(){ 
+        console.log('baz'); 
+    } 
+    return { //返回值对象实质上是一个 我们模块的公有API 
+        baz:baz 
+    } 
+} 
+
+var m=foo().baz(); //调用外部函数--->val1
+//调用函数的内部函数--->baz
+
+
+/*
+var a=10;
+function numberone(){
+    var b=20;
+    console.log(b);//20
+    function numbertwo(){
+        var c=30;
+        console.log(c);//由于函数不能在外部环境被调用，所以没有输出值
+    }
+    return { //返回值对象实质上是一个 我们模块的公有API 
+        numbertwo:numbertwo 
+    } 
+}
+var f=numberone();
+f.numbertwo();//不能再外部环境搜索到函数
+
+/*
+button = text("当前登录").findOne(500);
+if (button) {
+    let currentuser = button.parent().child(button.indexInParent() - 1).text();
+
+    console.log("当前用户" + currentuser);
+}
+if ('22222' == '22222') {
+    console.log('aaa');
+
+} else {
+    console.log('bbb');
+
+}
+/*
+
+result = {'a':'123','b':'456'};
+for(var i in result){
+
+    console.log(i);
+    console.log(typeof(i));
+    }
+/*
+waitForActivity("com.taobao.mytaobao.setting.TaobaoSettingActivity");
+/*
+
+    app.startActivity({
+        action: "VIEW",
+        className: "com.taobao.mytaobao.setting.TaobaoSettingActivity",
+        packageName: "com.taobao.taobao",
+    });
+
+    while(true) {
+        click(236,1530,844,1734);
+        button=text("去集福气").findOne(500);
+        if (button) {
+            back();
+            break;
+        }
+    }
+*/
+
+/*
 sleep(5000);
 for (let index = 0; index < 100; index++) {
     //const element = array[index];
@@ -9,7 +283,7 @@ for (let index = 0; index < 100; index++) {
     sleep(500);
     click(409, 1655);
 }
-
+*/
 
 
 
--- 福年种福果.js
+++ 福年种福果.js
@@ -1,22 +1,100 @@
-auto.waitFor();
-
 var height = device.height;
 var width = device.width;
-console.log("\n设备宽" + width + "\n设备高" + height + "\n手机型号" + device.model + "\n安卓版本" + device.release)
-if (height == 0 || width == 0) {
-    console.error("设备宽高获取失败")
-    engines.myEngine().forceStop();
-} else {
-    setScreenMetrics(width, height);
-}
-
 var button = null;
 var flag = 0;
+var timeout = 3;
+var users = { "c***3": 0, "黑***5": 0, "芍***离": 0 };
 
-initView();
+main()
 
-browseClick();
+function main() {
 
+    initReady();
+
+    initView();
+
+    browseClick();
+
+    watering();
+
+    let ac = accountSwitch();
+    if (ac) {
+        return main()
+    }
+
+}
+
+function initReady() {
+    auto.waitFor();
+    console.log("\n设备宽" + width + "\n设备高" + height + "\n手机型号" + device.model + "\n安卓版本" + device.release);
+    if (height == 0 || width == 0) {
+        console.error("设备宽高获取失败");
+        engines.myEngine().forceStop();
+    }
+    else {
+        setScreenMetrics(1080, 2160);
+    }
+}
+
+function initView() {
+    initAPP();
+
+    button = findButton("阿里拍卖");
+    if (button) {
+        click(540, 1564, 1054, 1809);
+        sleep(500);
+    } else {
+        if (findButton("待付款")) {
+            button = findButton("首页");
+            if (button) {
+                click(button.bounds().centerX(), button.bounds().centerY());
+                sleep(500);
+                return initView();
+            }
+            else {
+                console.error("未成功进入首页");
+                engines.myEngine().forceStop();
+            }
+        } else {
+            console.error("未成功进入首页");
+            engines.myEngine().forceStop();
+        }
+    }
+    button = findButton("收下祝福");
+    if (button) {
+        button.click();
+        sleep(500);
+    }
+    button = findButton("立即查看");
+    if (button) {
+        button.click();
+        sleep(500);
+        back();
+        sleep(500);
+    }
+    button = findButton("合种");
+    if (button) {
+        click(882, 1530, 1080, 1734);
+        sleep(1000);
+    }
+    button = findButton("去签到");
+    if (button) {
+        click(button.bounds().centerX(), button.bounds().centerY());
+        sleep(500);
+    }
+
+}
+
+function initAPP() {
+    app.startActivity({
+        action: "VIEW",
+        className: "com.taobao.tao.TBMainActivity",
+        packageName: "com.taobao.taobao",
+    });
+    console.log("等待淘宝主页Activity");
+    waitForActivity("com.taobao.tao.TBMainActivity",1000);
+    //app.launchApp("手机淘宝")
+}
 
 function browseClick() {
     button = findButtonEnd("去浏览");
@@ -24,12 +102,10 @@ function browseClick() {
         button.click();
         let t = button.text();
         if (!t.includes("已完成") && !t.includes("浏览首页") && !t.includes("淘宝人生") && !t.includes("我的淘宝") && !t.includes("去首页")) {
-            sleep(500);
-            swipe(500, 1200, 500, 300, 1000);
-            sleep(500);
-            swipe(500, 1200, 500, 300, 1000);
-            sleep(500);
-            swipe(500, 1200, 500, 300, 1000);
+            for (let index = 0; index < 3; index++) {
+                sleep(500);
+                swipe(width / 2, height / 4 * 3, width / 2, height / 4, 1000);
+            }
             sleep(15000);
             back();
         }
@@ -37,19 +113,15 @@ function browseClick() {
             if (t.includes("浏览首页")) {
                 console.info(t);
                 sleep(18000);
+                for (let index = 0; index < 4; index++) {
+                    sleep(500);
+                    swipe(width / 2, height / 4, width / 2, height / 4 * 3, 1000);
+                }
                 sleep(500);
-                swipe(500, 500, 500, 1500, 1000);
-                sleep(500);
-                swipe(500, 500, 500, 1500, 1000);
-                sleep(500);
-                swipe(500, 500, 500, 1500, 1000);
-                sleep(500);
-                swipe(500, 500, 500, 1500, 1000);
-                sleep(500);
-
                 initView();
-
-                judgeTaskView("浏览首页");
+                if (judgeTaskView("浏览首页")) {
+                    return browseClick();
+                }
             }
         }
         button = findButtonEnd("去浏览");
@@ -64,85 +136,161 @@ function browseClick() {
         sleep(500);
         click(110, 244)
         sleep(500);
-
-        judgeTaskView("去换装");
+        if (judgeTaskView("去换装")) {
+            return browseClick();
+        }
     }
 
     button = findButtonEnd("去兑换");
     if (button) {
         button.click();
-        return browseClick();
-    }
-
-}
-
-function judgeTaskView(name) {
-    waitForActivity("com.taobao.browser.BrowserActivity");
-    button = findButton("x5000");
-    if (button) {
-        console.info(name + "成功");
-        return browseClick();
-    }
-    else {
-        flag++;
-        console.error("返回失败" + flag);
-        initView();
-        if (flag < 3) {
+        if (judgeTaskView("去兑换")) {
             return browseClick();
-        } else {
-            console.error("失败次数过多");
-            engines.myEngine().forceStop();
+        }
+    }
+
+    function judgeTaskView(name) {
+        //waitForActivity("com.taobao.browser.BrowserActivity");
+        button = findButton("x5000");
+        if (button) {
+            console.info(name + "成功");
+            return 1;
+        }
+        else {
+            flag++;
+            console.error("返回失败" + flag);
+            initView();
+            if (flag < 3) {
+                return 1;
+            } else {
+                console.error("失败次数过多");
+                engines.myEngine().forceStop();
+                return 0;
+            }
         }
     }
 
 }
 
-function initView() {
-    app.startActivity({
-        action: "VIEW",
-        className: "com.taobao.tao.TBMainActivity",
-        packageName: "com.taobao.taobao",
-    });
-    waitForActivity("com.taobao.tao.TBMainActivity");
-    /*
-    app.launchApp("手机淘宝")
-    waitForActivity("com.taobao.tao.TBMainActivity");
-    back();
-    for (let index = 0; index < 5; index++) {
-        let act = currentActivity();
-        if (act == "com.taobao.tao.TBMainActivity") {
-            break;
-        } else {
+function watering() {
+    console.info("开始浇水");
+    //点击关闭浏览活动
+    click(929, 538, 1047, 656);
+    sleep(500);
+    while (text("合种").exists()) {
+        click(236, 1530, 844, 1734);
+        button = text("去集福气").findOne(500);
+        if (button) {
+            console.info("浇水结束");
             back();
+            break;
+        }
+    }
+}
+
+function accountSwitch() {
+    for (let user in users) {
+        if (user == "") {
+            console.error("无账户");
+            return 0;
+        }
+    }
+
+    let act = currentActivity();
+    let tbmain = "com.taobao.tao.TBMainActivity";
+    if (act != tbmain) {
+        initAPP();
+    }
+
+    /*
+    function findandClick(str) {
+        button = findButton(str);
+        if (button) {
+            if (button.clickable()) {
+                click(button);
+                return 1;
+            } else {
+                click(button.bounds().centerX(), button.bounds().centerY());
+                sleep(500);
+                return 1;
+            }
+        }
+        else {
+            console.error(str + "未点击");
+            return 0;
         }
     }
     */
-    
-    button = findButton("首页");
-    if (button) {
-        click(540, 1564, 1054, 1809);
-        sleep(500);
-    }
-    button = findButton("收下祝福");
-    if (button) {
-        button.click();
-        sleep(500);
-    }
-    button = findButton("合种");
-    if (button) {
-        click(882, 1530, 1080, 1734);
-        sleep(1000);
-    }
-    button = findButton("去签到");
+
+    // 我的淘宝 首页 设置 clickable为true但是点了没用 有用的是里面的child(0) 但是clickable为false点不了
+    button = findButton("我的淘宝");
     if (button) {
         click(button.bounds().centerX(), button.bounds().centerY());
-        sleep(500);
+        sleep(1000);
+    }
+    else {
+        console.error("账户切换失败");
+        return 0;
+    }
+
+    button = findButton("设置");
+    if (button) {
+        click(button.bounds().centerX(), button.bounds().centerY());
+        sleep(1000);
+    }
+    else {
+        console.error("账户切换失败");
+        return 0;
+    }
+
+    waitForActivity("com.taobao.mytaobao.setting.TaobaoSettingActivity");
+
+    button = findButton("切换账户");
+    if (button) {
+        click(button.bounds().centerX(), button.bounds().centerY());
+        sleep(1000);
+    }
+    else {
+        console.error("账户切换失败");
+        return 0;
+    }
+
+    button = findButton("当前登录");
+    if (button) {
+        let currentuser = button.parent().child(button.indexInParent() - 1).text();
+        console.log("当前用户" + currentuser);
+        users[currentuser] = 1;
+        for (let user in users) {
+            if (users[user] == 0) {
+                console.info("切换账户" + user);
+                button = findButton(user);
+                if (button) {
+                    click(button.bounds().centerX(), button.bounds().centerY());
+                    sleep(500);
+                    console.info("切换账户成功");
+                    return 1;
+                }
+                else {
+                    console.error("账户切换失败");
+                    return 0;
+                }
+            }
+        }
+        console.info("完成所有账户");
+        back();
+        back();
+        home();
+        return 0;
+    }
+    else {
+        console.error("账户切换失败");
+        return 0;
     }
 }
 
 function findButton(word) {
     let button = null;
-    for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < timeout; index++) {
         button = desc(word).findOne(500);
         if (button) {
             break;
@@ -152,20 +300,12 @@ function findButton(word) {
             break;
         }
     }
-    if (button) {
-        console.log("获取" + word + "成功");
-        console.log(button.text());
-        console.log(button.bounds());
-        return button;
-    } else {
-        console.error("获取" + word + "失败");
-        return 0;
-    }
+    return findLog(button, word);
 }
 
 function findButtonEnd(word) {
     let button = null;
-    for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < timeout; index++) {
         button = descEndsWith(word).findOne(500);
         if (button) {
             break;
@@ -175,9 +315,28 @@ function findButtonEnd(word) {
             break;
         }
     }
+    return findLog(button, word);
+}
+
+function findButtonStart(word) {
+    let button = null;
+    for (let index = 0; index < timeout; index++) {
+        button = descStartsWith(word).findOne(500);
+        if (button) {
+            break;
+        }
+        button = textStartsWith(word).findOne(500);
+        if (button) {
+            break;
+        }
+    }
+    return findLog(button, word);
+}
+
+function findLog(button, word) {
     if (button) {
         console.log("获取" + word + "成功");
-        console.log(button.text());
+        console.log("Text = " + button.text());
         console.log(button.bounds());
         return button;
     } else {
